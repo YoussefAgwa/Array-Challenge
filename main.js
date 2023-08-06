0c0e91be82ea24e4af99f11f9d0be7c557b9c024
YoussefAgwa <93931792+YoussefAgwa@users.noreply.github.com>
2023-08-06 22:24:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+/*
+  Array Challenge
+*/
+
+let zero = 0;
+
+let counter = 3;
+
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+
+// Write Code Here
+
+// my.pop();
+// my.pop();
+// my.reverse();
+
+// my.slice(zero,++counter).reverse();  //another solution
+
+// console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
+
+
+
+// console.log(my.slice(++zero , counter).reverse()); // ["Elham", "Mazero"]
+
+// console.log(`${my[--counter].slice(zero , counter)}${my[++zero].slice(counter)}`); // "Elzero"  counter after --counter became === 2
+
+// console.log(`${my[++zero][++counter]}${my[zero][++counter].toUpperCase()}`); // "rO"
+
+//////////////done ^_^
